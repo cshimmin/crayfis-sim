237593a7bb2ce8d3ccd5eb47e9715ac31b8c841f
maxim-borisyak <Maxim.Borisyak@gmail.com>
2018-02-26 20:40:21 +0300

    --jobs option for configs.py

--- scripts/configs.py
+++ scripts/configs.py
@@ -35,7 +35,7 @@ def get_total_flux(path):
   ])
 
 
-def generate_configs(output_dir, ngen=int(1.0e+5), pixDepth=1, pixWidth=1.5, npix=5000, spectra_path=None):
+def generate_configs(output_dir, ngen=int(1.0e+5), pixDepth=1, pixWidth=1.5, npix=5000, spectra_path=None, jobs=1):
   hist_dir = get_dir('../data/background_spectra')
   hists = [
     osp.join(hist_dir, item)
@@ -71,10 +71,13 @@ def generate_configs(output_dir, ngen=int(1.0e+5), pixDepth=1, pixWidth=1.5, npi
       beamEnergy=-1,
       particle_meta=[
         dict(
-          output=osp.join(output_dir, osp.basename(hist)[:-len('.root')]),
+          output=osp.join(output_dir, '%s_%04d' % (osp.basename(hist)[:-len('.root')], job)),
           particle=particle_name,
-          energyHisto=hist
-        ) for particle_name, hist in zip(particle_names, runtime_hists)
+          energyHisto=hist,
+          job=job
+        )
+        for particle_name, hist in zip(particle_names, runtime_hists)
+        for job in range(jobs)
       ],
       ngen=ngen,
       pixDepth=pixDepth,
@@ -100,15 +103,18 @@ def get_seeds(n, super_seed):
 
 if __name__ == '__main__':
   import argparse
+  import shutil as sh
 
   parser = argparse.ArgumentParser(description='Generate configs for cosmic background simulation.')
-  parser.add_argument('ngen', type=str, help='number of particles to generate')
+  parser.add_argument('ngen', type=str, help='number of particles of each type to shoot')
   parser.add_argument('-o', '--output', default='./', type=str, help='simulation output directory')
   parser.add_argument('-d', '--pixDepth', default=1.0, type=float, help='pixel depth')
   parser.add_argument('-w', '--pixWidth', default=1.5, type=float, help='pixel width')
   parser.add_argument('-n', '--npix', default=5000, type=float, help='linear size of the sensor')
-  parser.add_argument('-r', '--runtime_spectra_path', default='data/background_spectra/', type=str, help='overrides path to spectra files.')
-  parser.add_argument('-s', '--super_seed', default=12345, type=int, help='seed to generate seeds.')
+  parser.add_argument('-r', '--runtime_spectra_path', default='data/background_spectra/', type=str, help='overrides path to spectra files')
+  parser.add_argument('-s', '--super_seed', default=12345, type=int, help='seed to generate seeds')
+  parser.add_argument('-j', '--jobs', default=1, type=int, help='split simulation between number of jobs (per particle type)')
+  parser.add_argument('-c', '--configs_output', default='./configs/', type=str, help='output for the generated config files')
 
   args = parser.parse_args()
 
@@ -122,14 +128,22 @@ if __name__ == '__main__':
     pixDepth = args.pixDepth,
     pixWidth = args.pixWidth,
     npix = args.npix,
+    jobs=args.jobs,
     spectra_path=args.runtime_spectra_path
   ))
+
+  try:
+    os.makedirs(args.configs_output)
+  except OSError:
+    pass
 
   super_seed = args.super_seed
 
   for values, (seed1, seed2) in zip(configs, get_seeds(len(configs), super_seed=super_seed)):
     particle = values['particle']
+    job = values['job']
     values['seed1'] = seed1
     values['seed2'] = seed2
-    with open('%s.mac' % particle, 'w') as f:
+
+    with open(osp.join(args.configs_output, '%s_%04d.mac' % (particle, job)), 'w') as f:
       f.write(config.substitute(values))
